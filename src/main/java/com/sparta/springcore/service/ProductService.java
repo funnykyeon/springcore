078a0c10a385be1b49b388056e429359405cdd9f
funnykyeon@naver.com <funnykyeon@naver.com>
2022-04-16 14:37:46 +0900

    adminpage

--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -5,11 +5,11 @@ import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.model.Product;
 import com.sparta.springcore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -19,14 +19,14 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
+// 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto, userId);
 
-    public Product createProduct(ProductRequestDto requestDto) {
-        // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
         productRepository.save(product);
+
         return product;
     }
-
 
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         Product product = productRepository.findById(id)
@@ -39,10 +39,12 @@ public class ProductService {
         return product;
     }
 
-
-    public List<Product> getProducts() {
-        List<Product> products = productRepository.findAll();
-
-        return products;
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
+    }
+    //관리자용 상품 전체 조회
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
